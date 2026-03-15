811656d5af390e7c8d43b464b856b438371876f8
Ibilola Abiola <ibilolaabiola75@gmail.com>
2026-03-16 00:50:31 +0100

    Add test route to user.route.js

--- Src/routes/user.route.js
+++ Src/routes/user.route.js
@@ -8,5 +8,9 @@ const {validateRegisterUser, validateLoginUser} = require("../Validation/user.va
 router.post("/sign-up", validateRegisterUser,registerUser)
 router.post("/login", validateLoginUser, loginUser)
 
+router.get("/test", (req, res) => {
+  res.json({ message: "User route working" });
+});
+
 
 module.exports = router
